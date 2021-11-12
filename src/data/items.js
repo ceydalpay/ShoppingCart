98dea5e3b2c54b6536e7d105b4531fc9ca0b9eb3
CEYDA ALPAY <ceyda.alpay.6@gmail.com>
2021-11-12 13:47:26 +0300

    Editting items

--- src/data/items.js
+++ src/data/items.js
@@ -23,16 +23,16 @@ const items = [
     {
         id: 4,
         name: "The Picture of Dorian Gray",
-        price: 120.00,
+        price: 18.00,
         category: "Oscar Wilde",
         description: "Oscar Wilde's tale of a Faustian pact in Victorian England, The Picture of Dorian Gray is a both a slow-burning Gothic horror and a brilliant philosophical investigation of youth, beauty and desire. This Penguin Classics edition is edited with an introduction and notes by Robert Mighall. Enthralled by his own exquisite portrait, Dorian Gray exchanges his soul for eternal youth and beauty. Influenced by his friend Lord Henry Wotton, he is drawn into a corrupt double life; indulging his desires in secret while remaining a gentleman in the eyes of polite society. Only his portrait bears the traces of his decadence. The Picture of Dorian Gray was a succes de scandale. Early readers were shocked by its hints at unspeakable sins, and the book was later used as evidence against Wilde at the Old Bailey in 1895. It has lost none of its power to fascinate and disturb. This definitive edition includes a selection of contemporary reviews condemning the novels immorality, and the introduction to the first Penguin Classics edition by Peter Ackroyd. Oscar Wilde (1854-1900), son of an eminent eye-surgeon and a nationalist poet, was educated in Dublin and Oxford and became the leading exponent of the new Aesthetic Movement. His work, including short fiction such The Happy Price (1888), his novel The Picture of Dorian Gray (1891), gradually won him a reputation, which was cemented by his phenomenally successful plays, including A Woman of No Importance (1893), An Ideal Husband (1895) and The Importance of Being Earnest (1895). Imprisoned for homosexual acts, he died after his release, in exile in Paris. If you enjoyed The Picture of Dorian Gray, you might like Joris-Karl Huysmans' Against Nature (A Rebours) Wilde's real-life inspiration for the novel that slowly corrupts Dorian Gray, also available in Penguin Classics."
     },
     {
         id: 5,
-        name: "Winter Parka",
-        price: 230.50,
-        category: "Jackets",
-        description: "Sed convallis gravida ultricies. Suspendisse venenatis, est nec viverra laoreet, justo quam ullamcorper dui, sit amet iaculis diam orci id odio."
+        name: "Little Women",
+        price: 8.50,
+        category: "Louisa May Alcott",
+        description: "The charming story of four 'little women' - Meg, Jo, Amy and Beth - and their wise and patient mother Marmee, was an instant success when first published in 1868. Enduring hardships and enjoying adventures in Civil War New England, the March sisters have been adored for generations. Readers have rooted for Laurie in his pursuit of Jo's hand, cried over little Beth's death, and dreamed of travelling through Europe with old Aunt March and Amy. Future writers have found inspiration in Jo's devotion to her writing. In this simple, enthralling tale, both parts of which are included here, Louisa May Alcott has created four of American literature's most beloved women. In her enlightening, thoughtful introduction, Elaine Showalter discusses Louisa May Alcott's influences, and her aspirations for Little Women, as well as the impact the novel has had on such women writers as Joyce Carol Oates and Cynthia Ozick. This edition also includes notes on the text by Siobhan Kilfeather and Vinca Showalter. Louisa May Alcott (1832-1888) grew up surrounded by American writers such as Ralph Waldo Emerson, Nathaniel Hawthorne, and Henry David Thoreau. Drawing on her experience as a volunteer nurse in the Union Army during the American Civil War, she published Hospital Sketches in 1863, followed by gothic romances and lurid thrillers such as A Modern Mephistopheles and A Long Fatal Love Chase. In 1868, she published Little Women, which proved so popular that it was followed by two sequels."
     }
 ]
 
